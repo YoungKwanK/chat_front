d77c4fa101a47cbf402fa3d9a9160feeb1c4f4b1
YoungKwanK <young.kwin@gmail.com>
2025-08-04 09:08:54 +0900

    chatting-front

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHashHistory} from "vue-router"
+import MemberCreate from "@/views/MemberCreate.vue"
+import LoginPage from "@/views/LoginPage.vue";
+import MemberList from "@/views/MemberList.vue"
+import SimpleWebSocket from "@/views/SimpleWebSocket.vue"
+
+const routes = [
+  {
+    path:'/member/create',
+    name: 'MemberCreate',
+    component: MemberCreate
+  },
+  {
+    path:'/login',
+    name: 'LoginPage',
+    component: LoginPage
+  },
+  {
+    path:'/member/list',
+    name: 'MemberList',
+    component: MemberList
+  },
+  {
+    path:'/simple/chat',
+    name: 'SimpleWebSocket',
+    component: SimpleWebSocket
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+});
+
+export default router;
